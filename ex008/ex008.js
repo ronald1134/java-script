e1edb87ec49d532805eea17b23bf7715ddcc4b8a
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-02-09 18:51:27 -0300

    atualizando ex's de Js

---
+++ ex008/ex008.js
@@ -0,0 +1,7 @@
+
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!😡`)
+}
+console.log(`dirija sempre usando cinto de segurança!`)
---
+++ ex008/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+if (país == 'Brasil'){
+    console.log('você é brasileiro🏖️')
+}else{
+    console.log('você é estrangeiro🗽')
+}
+console.log(`vivendo em ${país}`)
