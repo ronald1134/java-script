80a75eac5f633218017249730b412ac805ff7d7e
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-02-16 16:30:48 -0300

    atualizando ex práticos

--- ex14_prática copy/script.js
+++ ex14_prática copy/script.js
@@ -2,16 +2,22 @@ function carregar(){
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 18
+    var hora = data.getHours()
+    //var hora = 18
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora >= 0 && hora <12){
         img.src = 'image/foto_manhã.jpg'
-    }else if (hora >= 12 && hora <17){
+        document.body.style.background = '#74a7d4'
+    }else if (hora >= 12 && hora <19){
         img.src = 'image/foto_tarde.jpg'
+        document.body.style.background = '#e8a65a'
     }else{
         img.src = 'image/foto_noite.jpg'
+        document.body.style.background = '#273119'
     }
 }
 
+//cores (manhã #74a7d4)
+//cores (tarde #e8a65a)
+//cores (noite #273119)
