67650bbe0a11d6a1982057c4a3d27d2f5dd96fa7
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-03-02 20:19:19 -0300

    atualizando exercícios Js

--- ex12_prática/script.js
+++ ex12_prática/script.js
@@ -9,28 +9,43 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(form.value)
-        var genero = ''
+        var gênero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-
-        if(form[0].checked){
-            genero = 'homem'
-            if (idade >=0 && idade <10){
-                //criança
+        img.setAttribute("style", "border-radius: 50%; object-fit: cover; width: 200px; height: 200px; object-position: center top;");
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //crianca
                 img.setAttribute('src', 'image/bebe_m.jpg')
-            }else if(idade <20){
+            } else if (idade < 21) {
                 //jovem
-                img.setAttribute('src', 'image/jovem_m.jpg')
-            }else if(idade <50){
+                img.setAttribute('src', 'image/jovem_m.jpeg')
+            } else if (idade < 50) {
                 //adulto
                 img.setAttribute('src', 'image/adulto_m.jpg')
-            }else{
+            } else {
                 //idoso
                 img.setAttribute('src', 'image/idoso_m.jpg')
             }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //crianca
+                img.setAttribute('src', 'image/bebe_f.jpg')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'image/jovem_f.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'image/adulto_f.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'image/idoso_f.jpg')
+            }
         }
-
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com idade ${idade} anos.`
         res.appendChild(img)
     }
 }
