8167298dcaed8489040b60c7cacfa6dcbf393038
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-03-04 15:07:56 -0300

    loop com while e DO while

---
+++ ex14_loop/ambiente.js
@@ -0,0 +1,24 @@
+//laço de repetição com testes lógicos final
+var c = 1
+do {
+    console.log(`Passos ${c}`)
+    c++
+} while (c <= 2)
+
+/*
+
+//laço de repetição com testes lógicos inicial
+var c = 1
+while(c <= 6){
+    console.log(`Passos ${c}`)
+    c++
+}
+    
+*/
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
