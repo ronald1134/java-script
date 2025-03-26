3b5434fec8072908de3ab398986eff9269ec3d80
Ronald Oliveira <164264112+ronald1134@users.noreply.github.com>
2025-03-26 19:31:21 -0300

    aprendendo array

---
+++ ex17_array/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5,2,9,8,4,1]
+num.push(3)
+num.sort()
+console.log(num)
+console.log(`nosso vetor contém ${num.length} elementos`)
+console.log(`o primeiro elemento do vetor ${num[0]}`)
+
+
+//num[3] = 6  //acrescentando um elemento sem perder os valores anteriores 
+//num.push(7) //acrescentando um elemento no final do vetor
+//num.length  //quantidade de elementos do vetor
+//num.sort()  //ordenando os elementos do vetor    
